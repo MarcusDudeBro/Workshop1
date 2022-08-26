97ae16013afaf2c999754c5b70c4f9f9c1019449
Marcus Casillas <53356104+MarcusDudeBro@users.noreply.github.com>
2022-08-26 11:25:22 -0700

    add main workshop code

---
+++ main.py
@@ -0,0 +1,42 @@
+import pyautogui
+import cv2
+import mediapipe as mp
+import keyboard
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
+s = pyautogui.size()
+
+# Initialize webcam
+cap = cv2.VideoCapture(1)
+while True:
+    # capture webcame frame and shape (width and height)
+    ret, frame = cap.read()
+    x, y, c = frame.shape
+    # defining the frame
+    frame = cv2.flip(frame, 1)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # have mediapipe hands predict hand landmarks
+    result = hands.process(framergb)
+    # iterate through the predicted landmarks adjusting them to the window, and 
+    # and outputting them to the opencv window
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            # take the 8th landmark (index finger point) and move the cursor to that landmarks x and y value
+            pyautogui.moveTo(int(handslms.landmark[8].x * s[0]), int(handslms.landmark[8].y * s[1]), _pause=False)
+            for lm in handslms.landmark:
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+                landmarks.append([lmx, lmy])
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+    cv2.imshow('Output', frame)
+    # if q is pressed, program exits
+    if cv2.waitKey(1) == ord('q'):
+        break
+    if keyboard.is_pressed('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
